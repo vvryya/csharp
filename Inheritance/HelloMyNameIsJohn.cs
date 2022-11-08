bcc5dd72bd42793278c055e14e609a5d511cc750
Varya <pvarua@gmail.com>
2022-11-09 02:44:46 +0300

    inheritance

---
+++ Inheritance/HelloMyNameIsJohn.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Inheritance
+{
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+    class Professional : Person
+    {
+        public string Profession { get; set; }
+    }
+
+    class Programmer : Professional
+    {
+        public Programmer()
+        {
+            Profession = "Programmer";
+        }
+        
+        public void OutputInfo()
+        {
+            Console.WriteLine($"Hello, my name is {Name}, I'm a {Profession}");
+        }
+    }
+
+    public class ProgrammerISAProfessional
+    {
+        public static void Main()
+        {
+            var programmer = new Programmer
+            {
+                Name = "John",
+            };
+
+            programmer.OutputInfo();
+        }
+    }
+}
---
+++ Inheritance/Shapes.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Inheritance
+{
+    class Shape
+    {
+        public string Name { get; set; }
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+    }
+
+    class Squar : Rectangle
+    {
+        public double Size { get; set; }
+    }
+
+    public class ShapesMain
+    {
+        public static void Main()
+        {
+            var square = new Squar
+            {
+                Name = "Pretty square",
+                Size = 12
+            };
+
+            Console.WriteLine($"We've created a square of size {square.Size}");
+        }
+    }
+}
